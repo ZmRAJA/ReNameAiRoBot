4971dc674d205c8fe9764d1d8b42465803f9a56e
Dr DanG <75981442+zmraj@users.noreply.github.com>
2021-12-01 21:42:40 +0530

    Update en.py

--- RenameAibot/translations/en.py
+++ RenameAibot/translations/en.py
@@ -2,7 +2,7 @@ class EnTrans:
     
     WRONG_VALUE_ERROR = "Invalid value entered for {} variable."
     
-    START_MSG = "Hello there, I am Mesh Rename Bot. Open Source at https://github.com/yash-dk/Mesh-RenameBot/tree/master. You can deploy your own."
+    START_MSG = "Hello there, I am Mesh Rename Bot. Open Source at https://github.com/zmraj/renamerobot.git. You can deploy your own."
      
     CANCEL_MESSAGE = "The rename has been canceled. Will be updated soon."
     
